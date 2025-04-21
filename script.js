8620ce64f087919a4fe38c41d6195a91e9dddda5
LucasSantos96 <114953327+LucasSantos96@users.noreply.github.com>
2025-04-21 18:49:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let aleatorio = Math.floor(Math.random() * 101);
+let tentativas = 6;
+
+const numEscolhido = document.getElementById('numEscolhido');
+const btn = document.getElementById('btnEscolha');
+const resultado = document.getElementById('resultado');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const valor = Number(numEscolhido.value);
+
+  if (valor === aleatorio) {
+    resultado.innerText = 'Você acertou!😍';
+    resultado.style.color = 'green';
+    btn.disabled = true;
+    reiniciarJogo(); // chama a função para reiniciar
+  } else {
+    tentativas--;
+    if (tentativas > 0) {
+      resultado.innerText = `Você errou.😰\n Ainda tem ${tentativas} tentativas.`;
+      resultado.style.color = 'red';
+    } else {
+      resultado.innerText = `Você perdeu!😭 \n O número era ${aleatorio}`;
+      resultado.style.color = 'gray';
+      btn.disabled = true;
+      reiniciarJogo();
+    }
+  }
+
+  numEscolhido.value = '';
+});
+
+function reiniciarJogo() {
+  setTimeout(() => {
+    aleatorio = Math.floor(Math.random() * 101);
+    tentativas = 6;
+    resultado.innerText = 'Novo jogo iniciado! Tente novamente.';
+    resultado.style.color = 'black';
+    btn.disabled = false;
+  }, 3000); // espera 3 segundos antes de reiniciar
+}
